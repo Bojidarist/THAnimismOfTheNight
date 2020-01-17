9260c6e61677abe9a78b0aa551e40c27c9da590f
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-01-17 23:31:00 +0200

    WIP Spawner script

--- TouhouAnimismOfTheNight/Assets/Scripts/Utilities/Config.cs
+++ TouhouAnimismOfTheNight/Assets/Scripts/Utilities/Config.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace TH.Utilities
 {
@@ -6,10 +7,12 @@ namespace TH.Utilities
     {
         public static float SFXVolume = 0.2f;
         public static float MusicVolume = 1f;
-        public static float GenericMovementMultiplier = 7.5f;
+        public static float GenericMovementMultiplier = 4f;
         public static float FocusMovementMultiplier = 0.2f;
         public static float FocusHitboxSize = 0.1f;
         public static Vector2 FocusHitboxSize2D = new Vector2(FocusHitboxSize, FocusHitboxSize);
         public static KeyCode FocusMovementKey = KeyCode.LeftShift;
+
+        public static string SakuyaPrefabPath = Path.Combine("Prefabs", "Sakuya");
     }
 }
---
+++ TouhouAnimismOfTheNight/Assets/Scripts/Utilities/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TH.Utilities
+{
+    public class Spawner : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Test with spawning player
+            // Make sure the methods are called from another place
+            // like GameManager
+            SpawnPlayer(Vector3.zero);
+        }
+
+        public static GameObject SpawnPlayer(Vector3 position)
+        {
+            var prefab = Resources.Load<GameObject>(Config.SakuyaPrefabPath);
+            prefab = Instantiate(prefab, position, Quaternion.identity);
+            return prefab;
+        }
+    }
+}
